5f2447581c302c361408103e05f08d64b2e7b5d0
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-11-03 11:07:51 -0600

    Add packages, burn film

--- src/pages/api/refresh.ts
+++ src/pages/api/refresh.ts
@@ -1,14 +1,37 @@
 import { getExpiredGMs, getExpiredFilm } from "@app/features/getExpiredGMs";
+import getJsonRpcProvider from "@app/features/getJsonRpcProvider";
 import { NextApiHandler } from "next";
+import { GmCam__factory } from "sdk/";
+import { ethers } from "ethers";
 
 const api: NextApiHandler = async (_req, res) => {
   // fetch expired gms & film from subgraph
   const { gms: expiredGms } = await getExpiredGMs();
-  console.log({ expiredGms });
+  // TODO: types
+  const expiredGmIds = expiredGms.map((gm: any) => gm.id);
+
   const { gmfilms: expiredFilm } = await getExpiredFilm();
-  console.log({ expiredFilm });
-  // TODO: pass to burnExpired on GM.sol
-  return res.json({ success: true });
+  const expiredGmFilmIds = expiredFilm.map((film: any) => film.id);
+
+  if (process.env.PRIVATE_KEY && process.env.GM_CAM_ADDRESS) {
+    const signer = new ethers.Wallet(
+      process.env.PRIVATE_KEY,
+      getJsonRpcProvider()
+    );
+
+    // pass to burnExpired on GM.sol
+    const gmCam = GmCam__factory.connect(process.env.GM_CAM_ADDRESS, signer);
+    let toBurn = expiredGmIds.concat(expiredGmFilmIds);
+
+    if (toBurn.length > 0) {
+      console.log("burning tokens", toBurn);
+      await gmCam.burnExpired(toBurn);
+    }
+
+    return res.json({ success: true });
+  } else {
+    return res.json({ success: false });
+  }
 };
 
 export default api;
