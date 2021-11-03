0b24a775fbeebf226e7b8e48c69da47af207d79f
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-11-03 11:08:03 -0600

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const IndexPage = () => {
   return (
     <div className="index">
-      Index
+      gm server
       <style jsx>{`
         .index {
         }
