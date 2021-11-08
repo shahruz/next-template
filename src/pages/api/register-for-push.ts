9a35df58177af14dae123d44120d41a83bba3fa3
shahruz <hello@shahr.uz>
2021-11-07 21:20:47 -0800

    fix push update

--- src/pages/api/register-for-push.ts
+++ src/pages/api/register-for-push.ts
@@ -4,11 +4,10 @@ import { NextApiHandler } from 'next';
 const api: NextApiHandler = async (req, res) => {
   const { address, token }: { address: string; token: string } = req.body;
   await prisma.pushTokens.deleteMany({ where: { token } });
-  await prisma.pushTokens.create({
-    data: {
-      id: address.toLowerCase(),
-      token
-    }
+  await prisma.pushTokens.upsert({
+    where: { id: address.toLowerCase() },
+    create: { id: address.toLowerCase(), token },
+    update: { token }
   });
   return res.json({ success: true });
 };
